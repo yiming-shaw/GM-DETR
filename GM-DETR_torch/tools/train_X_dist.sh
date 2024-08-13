523407dc3717eb4c201e9acb2176f2477c83e456
yimingshaw <ymxiao@std.uestc.edu.cn>
2024-08-13 23:13:48 +0800

    change sh

--- GM-DETR_torch/tools/train_X_dist.sh
+++ GM-DETR_torch/tools/train_X_dist.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=0,1
 torchrun --nproc_per_node=2 tools/train_X.py -c configs/gmdetr/gmdetr_r50vd_6x_align_flir_X_train1.yml
 
 torchrun --nproc_per_node=2 tools/train_X.py -c configs/gmdetr/gmdetr_r50vd_6x_align_flir_X_train2.yml \
--r /data0/XYM/GM-DETR_pytorch/output/gmdetr_r50vd_6x_align_flir_X_train1/checkpoint0019.pth
+-r output/gmdetr_r50vd_6x_align_flir_X_train1/checkpoint0019.pth
